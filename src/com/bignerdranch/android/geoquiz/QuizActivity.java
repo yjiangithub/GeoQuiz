4ec8c85777b82cf6cf13efbb7e1d3ed3d77cfc68
yjian <568309226@qq.com>
2017-12-14 00:23:48 +0800

    002

--- src/com/bignerdranch/android/geoquiz/QuizActivity.java
+++ src/com/bignerdranch/android/geoquiz/QuizActivity.java
@@ -61,6 +61,7 @@ public class QuizActivity extends ActionBarActivity {
 		int id = item.getItemId();
 		if (id == R.id.action_settings) {
 			return true;
+			
 		}
 		return super.onOptionsItemSelected(item);
 	}
